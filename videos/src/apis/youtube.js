f3a5c0bf1c665b545d68311cccddaba301f6317f
LBU <lbu@LBUui-MacBook-Pro.local>
2020-10-21 21:45:56 -0400

    second commit

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,3 +1,13 @@
-
+import axios from 'axios'
 
 const KEY = 'AIzaSyADrKwP3DV1ipcHL-cZSD3i8T-knyj4-ds'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+})
